f01044b8167b1fd3ca56eb9c02d12d95169d0380
Nick Rader <raderaust@aol.com>
2020-11-18 12:13:51 -0500

    Misc

--- Ccreator/Ccreator.Android/obj/Debug/90/designtime/Resource.designer.cs
+++ Ccreator/Ccreator.Android/obj/Debug/90/designtime/Resource.designer.cs
@@ -9380,19 +9380,13 @@ namespace Ccreator.Droid
 		{
 			
 			// aapt resource value: 0x7F0C0000
-			public const int AppIcon = 2131492864;
+			public const int icon = 2131492864;
 			
 			// aapt resource value: 0x7F0C0001
-			public const int Ccreator = 2131492865;
+			public const int icon_round = 2131492865;
 			
 			// aapt resource value: 0x7F0C0002
-			public const int icon = 2131492866;
-			
-			// aapt resource value: 0x7F0C0003
-			public const int icon_round = 2131492867;
-			
-			// aapt resource value: 0x7F0C0004
-			public const int launcher_foreground = 2131492868;
+			public const int launcher_foreground = 2131492866;
 			
 			static Mipmap()
 			{
